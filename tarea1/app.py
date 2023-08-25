ff0a2da2c88f63ff1d09c5124a39a24de7f9f375
Isaac_Ss <142519991+Isaac181222@users.noreply.github.com>
2023-08-25 16:12:05 -0500

    Update app.py

--- tarea1/app.py
+++ tarea1/app.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '¡Hola, mundo! Esta es mi primera API con Flask.'
+
+if __name__ == '__main__':
+    app.run()
